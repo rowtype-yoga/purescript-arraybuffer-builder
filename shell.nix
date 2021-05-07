e403acc6c5bbc3f5533ab516f0cfe6fd5d7a68d9
James Brock <jamesbrock@gmail.com>
2021-05-07 15:44:59 +0900

    Ready for v2
    Waiting for uint, float32, arraybuffer-types to get rid of their
    dependencies on generics-rep in bower.json manifest.

--- shell.nix
+++ shell.nix
@@ -1,7 +1,6 @@
 { pkgs ? import <nixpkgs> { } }:
 let
   easy-ps = import (builtins.fetchGit {
-    # url = "git@github.com:justinwoo/easy-purescript-nix.git";
     url = "https://github.com/justinwoo/easy-purescript-nix.git";
     ref = "master";
     rev = "dae91f43317fd5ff207e11ea6bf4b6130e4ba9fc";
@@ -13,9 +12,12 @@ pkgs.mkShell {
     easy-ps.spago
     easy-ps.pulp
     easy-ps.psc-package
-    # easy-ps.psc-publish
     pkgs.nodejs-14_x
     pkgs.nodePackages.bower
   ];
   LC_ALL = "C.UTF-8"; # https://github.com/purescript/spago/issues/507
+  # https://github.com/purescript/spago#install-autocompletions-for-bash
+  shellHook = ''
+    source <(spago --bash-completion-script `which spago`)
+  '';
 }
